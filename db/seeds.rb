8685035b1b4ddebaa32fe61ce49b44217acc9bb2
Muriel Bertile <mbertile@yahoo.fr>
2019-02-13 16:40:39 +0100

    my super yelp is complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+restaurant_category = ['chinese', 'italian', 'japanese', 'french', 'belgian']
+
+5.times do
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: restaurant_category.sample
+  )
+end
